aebdd02f0e44d602f8d491fdaa70b3d4f279df09
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-06-28 18:06:57 +0200

    dynamic profile page and user's liked and comment pages

---
+++ src/pages/users/[id]/comments.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Comments from "@/components/Comments";
+
+export default function UserComments() {
+  const [userComments, setUserComments] = useState([]);
+  const router = useRouter();
+
+  const { id } = router.query;
+  useEffect(() => {
+    async function fetchUserComments() {
+      try {
+        const response = await fetch(`/api/comments?userId=${id}`);
+        const data = await response.json();
+        if (data.comment) {
+          setUserComments(data.comment);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUserComments();
+  }, [id]);
+  console.log(userComments);
+  return (
+    <div>
+      <h1>User comments</h1>
+      <Comments comments={userComments} />
+    </div>
+  );
+}
