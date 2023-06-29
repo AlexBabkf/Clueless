b6cbad34382e1ee4089fd1191116d15ae84812fd
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-06-29 15:22:07 +0200

    arrow button to go again to top of the page

---
+++ src/components/TopButton/index.js
@@ -0,0 +1,32 @@
+import { StyledTopButton } from "./styledTopButton";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+export default function TopButton() {
+  const [isSeen, setIsSeen] = useState(false);
+
+  useEffect(() => {
+    function showButton() {
+      setIsSeen(window.scrollY < window.screen.height / 2);
+    }
+    window.addEventListener("scroll", showButton);
+
+    return () => {
+      window.removeEventListener("scroll", showButton);
+    };
+  }, []);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  return (
+    <StyledTopButton isSeen={isSeen} onClick={scrollToTop}>
+      <Image src="/top-arrow.png" alt="top-icon" width={10} height={10} />
+    </StyledTopButton>
+  );
+}
---
+++ src/components/TopButton/styledTopButton.js
@@ -0,0 +1,21 @@
+import styled, { css } from "styled-components";
+
+export const StyledTopButton = styled.button`
+  z-index: 2;
+  position: fixed;
+  cursor: pointer;
+  bottom: 55px;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 2px solid #484848;
+  opacity: ${(props) => (props.isSeen ? 0 : 1)};
+  outline: 0;
+
+  img {
+    width: 70%;
+    height: 70%;
+  }
+`;
